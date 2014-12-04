b312e65c5d6ec47a21b9eb7a99bbd807f53888b2
vlechemin <vlechemin@makemereach.com>
2014-12-04 22:05:46 +0100

    Remove CURL safe uploads for php 5.6

--- src/base_facebook.php
+++ src/base_facebook.php
@@ -957,6 +957,7 @@ abstract class BaseFacebook
     $opts = self::$CURL_OPTS;
     if ($this->getFileUploadSupport()) {
       $opts[CURLOPT_POSTFIELDS] = $params;
+      curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
     } else {
       $opts[CURLOPT_POSTFIELDS] = http_build_query($params, null, '&');
     }
